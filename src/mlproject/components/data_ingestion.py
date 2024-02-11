61d231135e25bda681707650966b0f327df05093
shariquemd <mdsharique.jmi@gmail.com>
2024-02-11 17:37:37 +0530

    data transformation

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -19,15 +19,15 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
         try:
-            df=read_sql_data()
+            df=pd.read_csv(os.path.join('notebook/data','stud.csv'))
             logging.info("Reading from mysql database")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_test_data_path,index=False,header=True)
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Data Ingestion is completed")
 
